fab5bd3f8cc579b32d16ad79f0cda80f82522c60
newforce42 <alx.fr@outlook.com>
2019-10-02 15:48:00 -0400

    added new navbar and login/logout buttons

--- react-nutshell/src/components/Nutshell.js
+++ react-nutshell/src/components/Nutshell.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import NavBar from "./nav/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "typeface-roboto";
+
 import ApplicationViews from "./ApplicationViews";
 
 // import "./Nutshell.css";
