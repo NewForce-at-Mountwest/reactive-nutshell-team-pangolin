c761ff5836e96c418ce3efd5de7bab5c28c9f2c9
LanceP51 <appleguy05@live.com>
2019-10-08 09:56:39 -0400

    commented. could style some more

--- react-nutshell/src/components/tasks/TaskList.js
+++ react-nutshell/src/components/tasks/TaskList.js
@@ -15,6 +15,7 @@ class TaskList extends Component {
     taskToEdit: {}
   };
 
+  // editing method to save upon enter key press with userId specific. Then use api manager to PUT and reGET all, and clear TaskToEdit object so edit fields go away
   updateTask = evt => {
       evt.preventDefault();
       if (evt.keyCode === 13)
@@ -35,6 +36,7 @@ class TaskList extends Component {
         });}
   };
 
+  // keeping state up to date with typing in changes
   handleFieldChange = evt => {
     const stateToChange = {};
     stateToChange[evt.target.id] = evt.target.value;
@@ -42,6 +44,7 @@ class TaskList extends Component {
   };
 
   componentDidMount() {
+    // getAll fetch call will get everything that is not archived
     TaskManager.getAll().then(tasks => {
       this.setState({
         tasks: tasks
@@ -49,6 +52,7 @@ class TaskList extends Component {
     });
   }
 
+  // unon ADD button, POST a new item to server
   buildTask = evt => {
     evt.preventDefault();
     if (this.state.task === "") {
@@ -73,6 +77,7 @@ class TaskList extends Component {
     }
   };
 
+  // after checkbox click, PATCH item on server to be archived
   handleCheckboxChange = (taskId) => {
     TaskManager.archive(taskId)
     .then(TaskManager.getAll)
@@ -89,6 +94,7 @@ class TaskList extends Component {
       <body id="task-body">
         <h1>Tasks</h1>
         <form>
+          {/* inputs for new task and date and ADD btn with a click evt to build method */}
           <fieldset>
             <div id="newTaskField">
             <div className="formgrid">
@@ -101,6 +107,7 @@ class TaskList extends Component {
               />
               <input
                 type="date"
+                // date type will allow us to see a pop up calendar to select date
                 required
                 onChange={this.handleFieldChange}
                 id="date"
@@ -121,6 +128,7 @@ class TaskList extends Component {
         </form>
         <section className="section-content"></section>
         <div className="tasks-list">
+          {/* edit fields show up in that space where the clicked <p> tage was if the new taskToEdit object from state matches the singleTask in the map */}
           {this.state.tasks.map(singleTask => {
             return this.state.taskToEdit.id === singleTask.id ? (
               <>
@@ -131,24 +139,29 @@ class TaskList extends Component {
                   onChange={this.handleFieldChange}
                   id="task"
                   value={this.state.task}
+                  // on enter keyup we will run the updateTask method
                   onKeyUp={this.updateTask}
                 />
                 <input
                   type="date"
+                  // date type will allow us to see a pop up calendar to select date
                   required
                   className="form-control"
                   onChange={this.handleFieldChange}
                   id="date"
                   value={this.state.date}
+                   // on enter keyup we will run the updateTask method here too
                   onKeyUp={this.updateTask}
                 />
               </>
             ) : (
               <>
+              {/* displaying tasks and checkbox  */}
               <div id="tasks-display">
                 <input id="checkbox" type="checkbox" onClick={()=>this.handleCheckboxChange(singleTask.id)}></input>
                 <p class="listItem"
                   id="task"
+                  // on click of the <p> tag, we will have option to set that clicked task as the taskToEdit empty object in state
                   onClick={() => this.setState({
                       taskToEdit: singleTask,
                       task: singleTask.task,
